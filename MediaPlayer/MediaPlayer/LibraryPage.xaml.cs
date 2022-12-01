fb4bd46446761a84540c542b046d9354561b6d97
alexjthomson1882 <alexthomson666@gmail.com>
2022-12-01 16:19:17 +0000

    Simplified LibraryPage

--- MediaPlayer/MediaPlayer/LibraryPage.xaml.cs
+++ MediaPlayer/MediaPlayer/LibraryPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls;
 using System.Collections.Generic;
 using System.ComponentModel;
 using Windows.UI.Xaml.Navigation;
+using MusicPlayer.Utility;
 
 namespace MusicPlayer {
 
@@ -58,113 +59,7 @@ namespace MusicPlayer {
         }
 
         private void MediaDataGrid_Sorting(object sender, DataGridColumnEventArgs e) {
-            // get column tag:
-            string columnTag = e.Column.Tag.ToString();
-            // calculate sort direction:
-            DataGridSortDirection? sortDirection;
-            switch (e.Column.SortDirection) {
-                case DataGridSortDirection.Ascending: sortDirection = DataGridSortDirection.Descending; break;
-                case DataGridSortDirection.Descending: sortDirection = null; break;
-                default: sortDirection = DataGridSortDirection.Ascending; break;
-            };
-            // sort columns:
-            switch (columnTag) {
-                case "TrackName": {
-                    if (sortDirection == DataGridSortDirection.Ascending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Title ascending
-                            select media
-                        );
-                    } else if (sortDirection == DataGridSortDirection.Descending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Title descending
-                            select media
-                        );
-                    } else {
-                        MediaDataGrid.ItemsSource = MediaList;
-                    }
-                    e.Column.SortDirection = sortDirection;
-                    break;
-                }
-                case "AlbumName": {
-                    if (sortDirection == DataGridSortDirection.Ascending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Album ascending
-                            select media
-                        );
-                    } else if (sortDirection == DataGridSortDirection.Descending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Album descending
-                            select media
-                        );
-                    } else {
-                        MediaDataGrid.ItemsSource = MediaList;
-                    }
-                    e.Column.SortDirection = sortDirection;
-                    break;
-                }
-                case "ArtistName": {
-                    if (sortDirection == DataGridSortDirection.Ascending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Artist ascending
-                            select media
-                        );
-                    } else if (sortDirection == DataGridSortDirection.Descending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Artist descending
-                            select media
-                        );
-                    } else {
-                        MediaDataGrid.ItemsSource = MediaList;
-                    }
-                    e.Column.SortDirection = sortDirection;
-                    break;
-                }
-                case "Duration": {
-                    if (sortDirection == DataGridSortDirection.Ascending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Duration ascending
-                            select media
-                        );
-                    } else if (sortDirection == DataGridSortDirection.Descending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.Duration descending
-                            select media
-                        );
-                    } else {
-                        MediaDataGrid.ItemsSource = MediaList;
-                    }
-                    e.Column.SortDirection = sortDirection;
-                    break;
-                }
-                case "DateAdded": {
-                    if (sortDirection == DataGridSortDirection.Ascending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.DateAdded ascending
-                            select media
-                        );
-                    } else if (sortDirection == DataGridSortDirection.Descending) {
-                        MediaDataGrid.ItemsSource = new ObservableCollection<AudioMedia>(
-                            from media in MediaList
-                            orderby media.DateAdded descending
-                            select media
-                        );
-                    } else {
-                        MediaDataGrid.ItemsSource = MediaList;
-                    }
-                    e.Column.SortDirection = sortDirection;
-                    break;
-                }
-            }
+            MediaDataGridUtility.HandleSortMediaDataGrid(MediaDataGrid, e.Column, MediaList);
         }
 
         #endregion
